45c032ffc48efebd228a2cdf6d3a57873add2a99
ZellDell <wendell.hingosa@gmail.com>
2024-08-16 07:56:58 +0800

    Added main program to consume weather service API
    - Configured to prompt user for city and display weather information
    - Using Integrated WeatherService, fetches and deserialize weather data
      in JSON and XML format
    - Added a simple error handling when city is not found.

--- ConsumingWebService/Program.cs
+++ ConsumingWebService/Program.cs
@@ -6,8 +6,46 @@ namespace ConsumingWebService
     {
         static async Task Main(string[] args)
         {
-           
-            
+
+            const string BaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+            const string ApiKey = "5698ad3d8b13f8f91ebdabbf7ed1fba0";
+
+
+            Console.WriteLine("WEATHER SERVICE");
+            Console.WriteLine("********************\n");
+
+            Console.WriteLine("Name of the City: ");
+            string City = Console.ReadLine();
+
+
+
+            WeatherService weatherService = new(BaseUrl, City, ApiKey);
+
+            var jsonWeather = await weatherService.GetWeatherJsonAsync();
+
+            var XmlWeather = await weatherService.GetWeatherXmlAsync();
+
+            if (jsonWeather == null || XmlWeather == null)
+            {
+                Console.WriteLine("City not Found!");
+                return;
+            }
+
+            Console.WriteLine("********************\n");
+            Console.WriteLine($"JSON DESERIALIZE\n");
+
+            Console.WriteLine($"City: {jsonWeather.Name}, {jsonWeather.Sys.Country}");
+            Console.WriteLine($"Temperature : {jsonWeather.Main.Temp} | {jsonWeather.Weather[0].Description}");
+
+
+
+            Console.WriteLine($"XML DESERIALIZE\n");
+
+            Console.WriteLine($"City: {XmlWeather.City.Name}, {XmlWeather.City.Country}");
+            Console.WriteLine($"Temperature : {XmlWeather.Temperature.Value} {XmlWeather.Temperature.Unit}| {XmlWeather.Clouds.Name}");
+
+            Console.WriteLine();
+
         }
     }
 }
